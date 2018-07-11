11cc222e6c3e05a69ed8c98f187d3a17d8c54477
Satsuki <g015c1116@gmail.com>
2018-07-11 11:15:25 +0900

    TabeleVIew実装

--- Journey/ViewController.swift
+++ Journey/ViewController.swift
@@ -9,17 +9,52 @@
 import UIKit
 import GoogleMaps
 
-class ViewController: UIViewController {
+class ViewController: UIViewController ,UITableViewDelegate, UITableViewDataSource{
 
   @IBOutlet weak var titleTextField: UITextField!
   @IBOutlet weak var subView: UIView!
+  @IBOutlet weak var spotTable: UITableView!
+  @IBOutlet weak var tableHeight: NSLayoutConstraint!
+  
+  var height = 46
+  var count = 0
+  var text:Array = ["スポットを追加"]
   
   let myFrameSize:CGSize = UIScreen.main.bounds.size
   
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return(text.count)
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
+    cell.textLabel?.text = text[indexPath.row]
+    
+    return(cell)
+  }
+  
+  func tableView(_ table: UITableView,didSelectRowAt indexPath: IndexPath) {
+    let selectedNumber = text[indexPath.row]
+    print(selectedNumber)
+    if(selectedNumber == "スポットを追加" && text.count != 21){
+      print("AAAA")
+      count += 1
+      let str = count.description
+      text.append(str)
+      height += 46
+      tableHeight.constant = CGFloat(height)
+      spotTable.reloadData()
+    } else {
+      text[0] = "これ以上追加できません"
+      spotTable.reloadData()
+    }
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+    
     let camera = GMSCameraPosition.camera(withLatitude: -33.868,longitude:151.2086, zoom:15)
-    let mapView = GMSMapView.map(withFrame: CGRect(x:0,y:60,width:myFrameSize.width,height:myFrameSize.height/2.5),camera:camera)
+    let mapView = GMSMapView.map(withFrame: CGRect(x:0,y:60,width:myFrameSize.width,height:300),camera:camera)
     let marker = GMSMarker()
     
     marker.icon = UIImage(named:"thumbs-up")
@@ -27,6 +62,10 @@ class ViewController: UIViewController {
     self.subView.addSubview(mapView)
     
     titleTextField.placeholder = "プラン名を入力"
+    
+    spotTable.delegate = self
+    spotTable.dataSource = self
+    tableHeight.constant = CGFloat(height)
     
   }
 
