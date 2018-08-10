64a297b05f5c324dd96dd28570580b53315dd44d
猪岡勝生 <ino@inookakatsuonomacbook-puro.local>
2018-08-10 16:12:49 +0900

    スポット詳細とスポット登録を作成

---
+++ Journey/DetailSpotViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DetailSpotViewController.swift
+//  Journey
+//
+//  Created by 猪岡勝生 on 2018/08/10.
+//  Copyright © 2018年 Swift-Biginners. All rights reserved.
+//
+
+import UIKit
+
+class DetailSpotViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
