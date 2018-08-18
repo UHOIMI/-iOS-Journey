46f7a8cab1c1ad1efa9f040b6a7a3cf2f436525e
Satsuki <g015c1116@gmail.com>
2018-08-18 22:56:29 +0900

    tabBar修正

--- Journey/PostViewController.swift
+++ Journey/PostViewController.swift
@@ -171,6 +171,53 @@ class PostViewController: UIViewController ,UITableViewDelegate, UITableViewData
     textView.inputAccessoryView = kbToolBar
   }
   
+  func createTabBar(){
+    let width = self.view.frame.width
+    let height = self.view.frame.height
+    //デフォルトは49
+    let tabBarHeight:CGFloat = 58
+    /**   TabBarを設置   **/
+    tabBar = TabBar()
+    if UIDevice().userInterfaceIdiom == .phone {
+      switch UIScreen.main.nativeBounds.height {
+      case 2436:
+        tabBar.frame = CGRect(x:0,y:height - tabBarHeight - 34.1,width:width,height:tabBarHeight)
+      default:
+        tabBar.frame = CGRect(x:0,y:height - tabBarHeight,width:width,height:tabBarHeight)
+      }
+    }
+    //バーの色
+    tabBar.barTintColor = UIColor.lightGray
+    //選択されていないボタンの色
+    tabBar.unselectedItemTintColor = UIColor.black
+    //ボタンを押した時の色
+    tabBar.tintColor = UIColor.black
+    //ボタンを生成
+    let home:UITabBarItem = UITabBarItem(title: "home", image: UIImage(named:"home.png")!.withRenderingMode(UIImageRenderingMode.alwaysOriginal), tag: 1)
+    let search:UITabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 2)
+    let favorites:UITabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 3)
+    let setting:UITabBarItem = UITabBarItem(title: "setting", image: UIImage(named:"settings.png")!.withRenderingMode(UIImageRenderingMode.alwaysOriginal), tag: 4)
+    //ボタンをタブバーに配置する
+    tabBar.items = [home,search,favorites,setting]
+    //デリゲートを設定する
+    tabBar.delegate = self
+    
+    self.view.addSubview(tabBar)
+  }
+  func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+    switch item.tag{
+    case 1:
+      print("１")
+    case 2:
+      print("２")
+    case 3:
+      print("３")
+    case 4:
+      print("４")
+    default : return
+      
+    }
+  }
   
   enum actionTag: Int {
     case action1 = 1
