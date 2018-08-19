87ef2912b1066b33d57bbd1d4f4907ed1decbe0e
猪岡勝生 <ino@inookakatsuonomacbook-puro.local>
2018-08-20 00:57:43 +0900

    スポット追加画面のレイアウトと一部機能を追加（未完全）

--- Journey/PutSpotViewController.swift
+++ Journey/PutSpotViewController.swift
@@ -7,12 +7,17 @@
 //
 
 import UIKit
+import GoogleMaps
+import GooglePlaces
+import CoreLocation
+import CoreMotion
+import Darwin
 
-class PutSpotViewController: UIViewController ,UITabBarDelegate{
+class PutSpotViewController: UIViewController, UITabBarDelegate, GMSMapViewDelegate, CLLocationManagerDelegate,  UITextFieldDelegate{
     
       private var tabBar:TabBar!
 
-    override func viewDidLoad() {
+    /*override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
@@ -23,18 +28,155 @@ class PutSpotViewController: UIViewController ,UITabBarDelegate{
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
+    }*/
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    /****************/
+    
+    var lat:Double = 0
+    var lng:Double = 0
+    
+    var aaa = -33.868
+    var bbb = 151.2086
+    
+    let top = "エラー"
+    let message = "名前が入力されていません"
+    let okText = "OK"
+    
+    var latitudeList: [Double] = []
+    var longitudeList: [Double] = []
+    var nameList: [String] = []
+    
+    var spotDataList : [ListSpotModel] = []
+    
+    var locationManager: CLLocationManager!
+    let motionManager = CMMotionManager()
+    
+    //@IBOutlet weak var mapButton: UIButton!
+    @IBOutlet weak var mapButton: UIBarButtonItem!
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var selectSpotButton: UIButton!
+    
+    let myFrameSize:CGSize = UIScreen.main.bounds.size
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let camera = GMSCameraPosition.camera(withLatitude: aaa,longitude:bbb, zoom:15)
+        let mapView = GMSMapView.map(withFrame: CGRect(x:0,y:UIApplication.shared.statusBarFrame.size.height +  (self.navigationController?.navigationBar.frame.size.height)!,width:myFrameSize.width,height:myFrameSize.height/3),camera:camera)
+        let marker = GMSMarker()
+        
+        marker.icon = UIImage(named:"thumbs-up")
+        marker.map = mapView
+        
+        self.view.addSubview(mapView)
+        
+        createTabBar()
     }
-    */
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted, .denied:
+            showAlert()
+            break
+        case .authorizedAlways, .authorizedWhenInUse:
+            break
+        }
+    }
+    @IBAction func tappedMapButton(_ sender: Any) {
+        if nameTextField.text == "" {
+            let alert = UIAlertController(title: top, message: message, preferredStyle: UIAlertControllerStyle.alert)
+            let okayButton = UIAlertAction(title: okText, style: UIAlertActionStyle.cancel, handler: nil)
+            alert.addAction(okayButton)
+            present(alert, animated: true, completion: nil)
+            return
+        }
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager = CLLocationManager()
+            locationManager.delegate = self as CLLocationManagerDelegate
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let newLocation = locations.last,
+            CLLocationCoordinate2DIsValid(newLocation.coordinate) else {
+                print("Error")
+                return
+        }
+        
+        let latitude:Double = atof("".appendingFormat("%.6f", newLocation.coordinate.latitude))
+        let longitude:Double = atof("".appendingFormat("%.6f", newLocation.coordinate.longitude))
+        
+        latitudeList.append(newLocation.coordinate.latitude)
+        longitudeList.append(newLocation.coordinate.longitude)
+        nameList.append(nameTextField.text!)
+        
+        var spotData : ListSpotModel =
+            ListSpotModel(la: newLocation.coordinate.latitude,lo: newLocation.coordinate.longitude,na: nameTextField.text!)
+        
+        spotDataList.append(spotData)
+        
+        lat = newLocation.coordinate.latitude
+        lng = newLocation.coordinate.longitude
+        
+        let camera = GMSCameraPosition.camera(withLatitude: latitude,longitude:longitude, zoom:15)
+        let mapView = GMSMapView.map(withFrame: CGRect(x:0,y:0,width:myFrameSize.width,height:myFrameSize.height/2),camera:camera)
+        self.view.addSubview(mapView)
+        
+        let marker: GMSMarker = GMSMarker()
+        
+        marker.position = camera.target
+        
+        marker.snippet = "Hello World"
+        
+        marker.map = mapView
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.stopUpdatingLocation()
+        }
+        
+    }
+    func showAlert() {
+        let alert = UIAlertController(title: "GPSがオフになっています", message: "設定を押して位置情報を許可してください", preferredStyle: UIAlertControllerStyle.alert)
+        let okAction = UIAlertAction(title: "設定", style: UIAlertActionStyle.default){(action: UIAlertAction) in
+            let url = NSURL(string: UIApplicationOpenSettingsURLString)!
+            UIApplication.shared.openURL(url as URL)
+        }
+        let cancelButton = UIAlertAction(title: "キャンセル", style: UIAlertActionStyle.cancel, handler: nil)
+        alert.addAction(cancelButton)
+        alert.addAction(okAction)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    
+    
+    @IBAction func tappedSelectSpotButton(_ sender: Any) {
+        goToSelectSpot()
+    }
+    
+    func goToSelectSpot(){
+        self.performSegue(withIdentifier: "goSelectSpot", sender:spotDataList)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let nextViewController = segue.destination as! SelectSpotViewController
+        nextViewController.spotDataList = sender as! [ListSpotModel]
+    }
+    
+    /****************/
     
     func createTabBar(){
         let width = self.view.frame.width
