e23b4306c2d72ec0057719ccb6f771467faf151e
Satsuki <g015c1116@gmail.com>
2018-10-30 16:01:00 +0900

    userPost実装

--- Journey/ConfirmationViewController.swift
+++ Journey/ConfirmationViewController.swift
@@ -20,8 +20,10 @@ class ConfirmationViewController: UIViewController {
   //var pickedImage: UIImage? //選択されたイメージを保存するためのインスタンス変数
   let boundary = "----WebKitFormBoundaryZLdHZy8HNaBmUX0d"
   
-  let ipAddress = "192.168.100.102:3000"
+  let ipAddress = "172.20.10.2:3000"
   //  let ipAddress = "35.200.26.70:443"
+  var gender:String = ""
+  var generation:Int = 0
   
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,12 +32,14 @@ class ConfirmationViewController: UIViewController {
       userGenerationLabel.text = globalVar.userGeneration
       userGenderLabel.text = globalVar.userGender
       setIconImageView.image = globalVar.userIcon
+
         // Do any additional setup after loading the view.
     }
   @IBAction func tappedModificationButton(_ sender: Any) {
     performSegue(withIdentifier: "backCreateUserView", sender: nil)
   }
   @IBAction func tappedStartViewButton(_ sender: Any) {
+    postImage()
     performSegue(withIdentifier: "toStartView", sender: nil)
   }
   
@@ -47,8 +51,24 @@ class ConfirmationViewController: UIViewController {
     fileUpload(url, data: body) {(data, response, error) in
       if let response = response as? HTTPURLResponse, let _: Data = data , error == nil {
         if response.statusCode == 200 {
+          var imageStr:String = String(data: data!, encoding: .utf8)!
+          if let range = imageStr.range(of: "["){
+            imageStr.removeSubrange(range)
+          }
+          for _ in 0...1{
+            if let range = imageStr.range(of: "\\"){
+              imageStr.removeSubrange(range)
+            }
+            if let range = imageStr.range(of: "\""){
+              imageStr.removeSubrange(range)
+            }
+          }
+          if let range = imageStr.range(of: "]"){
+            imageStr.removeSubrange(range)
+          }
           print("Upload done",data as Any)
           print(String(data: data!, encoding: .utf8) ?? "")
+          self.postUser(imageStr: imageStr)
         } else {
           print(response.statusCode)
         }
@@ -79,6 +99,78 @@ class ConfirmationViewController: UIViewController {
     let session = Foundation.URLSession(configuration: urlConfig)
     let task = session.uploadTask(with: request, from: data, completionHandler: completionHandler)
     task.resume()
+  }
+  
+  func postUser(imageStr:String){
+    settingData()
+    print("imageStr",imageStr)
+    let str = "user_id=\(globalVar.userId)&user_pass=\(globalVar.userPass)&user_name=\(globalVar.userName)&generation=\(generation)&gender=\(gender)&comment=こんにちは&user_icon=\(imageStr)"
+    let url = URL(string: "http://\(ipAddress)/api/v1/users/register")
+    var request = URLRequest(url: url!)
+    // POSTを指定
+    request.httpMethod = "POST"
+    // POSTするデータをBodyとして設定
+    request.httpBody = str.data(using: .utf8)
+    let session = URLSession.shared
+    session.dataTask(with: request) { (data, response, error) in
+      if error == nil, let data = data, let response = response as? HTTPURLResponse {
+        // HTTPヘッダの取得
+        print("Content-Type: \(response.allHeaderFields["Content-Type"] ?? "")")
+        // HTTPステータスコード
+        print("statusCode: \(response.statusCode)")
+        print(String(data: data, encoding: .utf8) ?? "")
+      }
+    }.resume()
+  }
+  
+  func settingData(){
+    switch userGenderLabel.text {
+    case "男性":
+      gender = "男"
+      break
+    case "女性":
+      gender = "女"
+      break
+    default:
+      break
+    }
+    switch userGenerationLabel.text {
+    case "10歳未満":
+      generation = 0
+      break
+    case "10代":
+      generation = 10
+      break
+    case "20代":
+      generation = 20
+      break
+    case "30代":
+      generation = 30
+      break
+    case "40代":
+      generation = 40
+      break
+    case "50代":
+      generation = 50
+      break
+    case "60代":
+      generation = 60
+      break
+    case "70代":
+      generation = 70
+      break
+    case "80代":
+      generation = 80
+      break
+    case "90代":
+      generation = 90
+      break
+    case "100歳以上":
+      generation = 100
+      break
+    default:
+      break
+    }
   }
 
     /*
