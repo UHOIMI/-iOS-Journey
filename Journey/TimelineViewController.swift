55534c65209097b853ca6e484a32011f5f77f9ea
Satsuki <g015c1116@gmail.com>
2018-11-29 15:35:12 +0900

    タイムライン作成(2件表示のみ)

--- Journey/TimelineViewController.swift
+++ Journey/TimelineViewController.swift
@@ -18,16 +18,18 @@ import RealmSwift
 class TimelineViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
   @IBOutlet weak var tableView: UITableView!
+  var ActivityIndicator: UIActivityIndicatorView!
+  let formatter = DateFormatter()
   
   let globalVar = GlobalVar.shared
   
-  var sampledatas = [1,2,3,4,5,6,7,8,9,10]
+  var sampledatas = [1,2]
   var isaddload:Bool = true
   var planIdList : [Int] = []
   var userIdList : [String] = []
   var planTitleList : [String] = []
   var userImagePathList : [String] = []
-  var dateList : [Date] = []
+  var dateList : [String] = []
   var spotImageSetFlag : [Int] = []
   var spotCountList : [Int] = []
   var spotNameListA : [String] = []
@@ -37,38 +39,64 @@ class TimelineViewController: UIViewController, UITableViewDelegate, UITableView
   override func viewDidLoad() {
     super.viewDidLoad()
     getPlan()
+//    tableView.reloadData()
     let refreshControl = UIRefreshControl()
     refreshControl.addTarget(self, action: #selector(TimelineViewController.refreshControlValueChanged(sender:)), for: .valueChanged)
     //tableView.register(UITableViewCell.self, forCellReuseIdentifier: "MyCell")
-    tableView.register(cellType: LoaddingTableViewCell.self)
-    let footerCell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "LoaddingTableViewCell")!
-    (footerCell as! LoaddingTableViewCell).startAnimationg()
-    let footerView: UIView = footerCell.contentView
-    tableView.tableFooterView = footerView
-    tableView.addSubview(refreshControl)
+//    tableView.register(cellType: LoaddingTableViewCell.self)
+//    let footerCell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "LoaddingTableViewCell")!
+//    (footerCell as! LoaddingTableViewCell).startAnimationg()
+//    let footerView: UIView = footerCell.contentView
+//    tableView.tableFooterView = footerView
+//    tableView.addSubview(refreshControl)
+    // ActivityIndicatorを作成＆中央に配置
+    ActivityIndicator = UIActivityIndicatorView()
+    ActivityIndicator.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+    ActivityIndicator.center = self.view.center
+    
+    // クルクルをストップした時に非表示する
+    ActivityIndicator.hidesWhenStopped = true
+    
+    // 色を設定
+    ActivityIndicator.style = UIActivityIndicatorView.Style.gray
+    
+    //Viewに追加
+    self.view.addSubview(ActivityIndicator)
+    ActivityIndicator.startAnimating()
     
   }
+
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(true)
+    tableView.reloadData()
+  }
+  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return sampledatas.count
+    return planTitleList.count
   }
   
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell: PlanTableViewCell = tableView.dequeueReusableCell(withIdentifier: "planCell", for : indexPath) as! PlanTableViewCell
     //let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "planCell", for: indexPath)
-    cell.planNameLabel.text = sampledatas[indexPath.row].description + "プラン名"
-    cell.planSpotNameLabel1.text = "スポット名"
-    cell.planSpotNameLabel2.text = "スポット名"
+    cell.planNameLabel.text = sampledatas[indexPath.row].description + planTitleList[indexPath.row]
+    cell.planSpotNameLabel1.text = spotNameListA[indexPath.row]
+    cell.planSpotNameLabel2.text = spotNameListA[indexPath.row]
     cell.planFavoriteLabel.text = 99999.description
     cell.planImageView.image = UIImage(named: "no-image.png")
-    cell.planUserIconImageView.image = UIImage(named: "no-image.png")
+    cell.planDateLabel.text = dateList[indexPath.row]
+    let iconUrl = URL(string: userImagePathList[indexPath.row])!
+    let iconData = try? Data(contentsOf: iconUrl)
+    cell.planUserIconImageView.image = UIImage(data:iconData!)
     cell.planUserIconImageView.layer.cornerRadius = 40 * 0.5
     print(cell.planUserIconImageView.frame.width)
     cell.planUserIconImageView.clipsToBounds = true
+//    cell.planDateLabel.text = String(date:dateList[indexPath.row])
     //cell.textLabel?.text = "\(sampledatas[indexPath.row])"
+    self.ActivityIndicator.stopAnimating()
     return cell
   }
   @objc func refreshControlValueChanged(sender: UIRefreshControl) {
@@ -84,7 +112,7 @@ class TimelineViewController: UIViewController, UITableViewDelegate, UITableView
     if (self.tableView.contentOffset.y + self.tableView.frame.size.height > self.tableView.contentSize.height && self.tableView.isDragging && isaddload == true){
       self.isaddload = false
       DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
-        for _ in 0..<2{
+        for _ in 0..<0{
           self.sampledatas.append(self.sampledatas.last! + 1)
         }
         if(self.sampledatas.count > 50){
@@ -135,6 +163,7 @@ class TimelineViewController: UIViewController, UITableViewDelegate, UITableView
       let spotIdR : Int?
       let spotIdS : Int?
       let spotIdT : Int?
+      let planDate : String
       let date : Date? = NSDate() as Date
       enum CodingKeys: String, CodingKey {
         case planId = "plan_id"
@@ -164,6 +193,7 @@ class TimelineViewController: UIViewController, UITableViewDelegate, UITableView
         case spotIdR = "spot_id_r"
         case spotIdS = "spot_id_s"
         case spotIdT = "spot_id_t"
+        case planDate = "plan_date"
         case date
       }
     }
@@ -226,7 +256,7 @@ class TimelineViewController: UIViewController, UITableViewDelegate, UITableView
         case spotImageA = "spot_image_a"
         case spotImageB = "spot_image_b"
         case spotImageC = "spot_image_c"
-        case date
+        case date = "date"
       }
       struct SpotAddress : Codable{
         let x : Double
@@ -256,7 +286,26 @@ class TimelineViewController: UIViewController, UITableViewDelegate, UITableView
             self.planIdList.append(planData!.record![i].planId)
             self.planTitleList.append(planData!.record![i].planTitle)
             self.userIdList.append(planData!.record![i].userId)
-            self.dateList.append(planData!.record![i].date!)
+            
+            var strDate = planData!.record![i].planDate
+            for _ in 0...1{
+              if let range = strDate.range(of: "-") {
+                strDate.replaceSubrange(range, with: "/")
+                print("日付:",strDate)
+              }
+            }
+            let prefixDate = strDate.prefix(11)
+            var suffixDate = prefixDate.suffix(6)
+            if let range = suffixDate.range(of: "/") {
+              suffixDate.replaceSubrange(range, with: "月")
+              print("日付:",suffixDate)
+            }
+            if let range = suffixDate.range(of: "T") {
+              suffixDate.replaceSubrange(range, with: "日")
+              print("日付:",suffixDate)
+            }
+            print("日付",planData!.record![i].planDate)
+            self.dateList.append(String(suffixDate))
             self.getUserImage(userId: self.userIdList[i], number: i)
             self.spotImageSetFlag.append(0)
             var count = 0
@@ -458,6 +507,9 @@ class TimelineViewController: UIViewController, UITableViewDelegate, UITableView
           print("画像",self.spotImagePathList!)
           print("A",self.spotNameListA)
           print("B",self.spotNameListB!)
+          if(number == 1){
+            self.tableView.reloadData()
+          }
         }
       }
     }.resume()
