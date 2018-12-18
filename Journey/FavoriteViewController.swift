e2e0a335eb74d0c9316a34acb048da40aa14b7b0
inoka <is091160@gmail.com>
2018-12-18 16:09:30 +0900

    地方の画像追加

---
+++ Journey/FavoriteViewController.swift
@@ -0,0 +1,32 @@
+//
+//  FavoriteViewController.swift
+//  Journey
+//
+//  Created by 猪岡勝生 on 2018/12/18.
+//  Copyright © 2018年 Swift-Biginners. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteViewController: UIViewController {
+  
+  @IBOutlet weak var planTable: UITableView!
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
