6e29f323a5274ab7fe2099d62f4471cf03f42e9a
Satsuki <f.825.tennis@gmail.com>
2019-02-07 15:12:52 +0900

    画像の呼び出しでURLSession使用

--- Journey/AppDelegate.swift
+++ Journey/AppDelegate.swift
@@ -347,10 +347,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             self.globalVar.newSpotImagePathList?.append("")
             self.globalVar.newTrueSpotImagePathList?.append(self.globalVar.newSpotImagePathList![0])
             if(self.globalVar.newTrueSpotImagePathList![i] != ""){
-              let url = URL(string: self.globalVar.newTrueSpotImagePathList![i])!
-              let imageData = try? Data(contentsOf: url)
-              let image = UIImage(data:imageData!)
-              self.globalVar.newSpotImageList?.append(image!)
+              self.imageUrl(imageURL: self.globalVar.newTrueSpotImagePathList![i],flag: 1)
+//              let url = URL(string: self.globalVar.newTrueSpotImagePathList![i])!
+//              let imageData = try? Data(contentsOf: url)
+//              DispatchQueue.main.async {
+//                let image = UIImage(data:imageData!)
+//                self.globalVar.newSpotImageList?.append(image!)
+//              }
+//
             }else{
               self.globalVar.newSpotImageList?.append(UIImage(named: "no-image.png")!)
             }
@@ -362,6 +366,26 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
       }else{
       }
     }.resume()
+  }
+  
+  func imageUrl(imageURL:String, flag:Int){
+    let url = URL(string: imageURL)
+    URLSession.shared.dataTask(with: url!, completionHandler: { (data, response, error) in
+      
+      if error != nil {
+        print(error!)
+        return
+      }
+      
+      DispatchQueue.main.async {
+        let image = UIImage(data: data!)!
+        if(flag == 1){
+          self.globalVar.newSpotImageList?.append(image)
+        }else if(flag == 2){
+          self.globalVar.searchSpotImageList?.append(image)
+        }
+      }
+    }).resume()
   }
   
   
@@ -424,10 +448,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             self.globalVar.searchSpotImagePathList?.append("")
             self.globalVar.searchTrueSpotImagePathList?.append(self.globalVar.searchSpotImagePathList![0])
             if(self.globalVar.searchTrueSpotImagePathList![i] != ""){
-              let url = URL(string: self.globalVar.searchTrueSpotImagePathList![i])!
-              let imageData = try? Data(contentsOf: url)
-              let image = UIImage(data:imageData!)
-              self.globalVar.searchSpotImageList?.append(image!)
+              self.imageUrl(imageURL: self.globalVar.searchTrueSpotImagePathList![i],flag: 2)
+//              let url = URL(string: self.globalVar.searchTrueSpotImagePathList![i])!
+//              let imageData = try? Data(contentsOf: url)
+//              DispatchQueue.main.async {
+//                let image = UIImage(data:imageData!)
+//                self.globalVar.searchSpotImageList?.append(image!)
+//              }
             }else{
               self.globalVar.searchSpotImageList?.append(UIImage(named: "no-image.png")!)
             }
@@ -659,11 +686,14 @@ class GlobalVar{
         
         self.postSpotImagePathList?.append("")
         self.postTrueSpotImagePath = self.postSpotImagePathList![0]
-        if(self.postTrueSpotImagePath != ""){
-          let url = URL(string: self.postTrueSpotImagePath)!
-          let imageData = try? Data(contentsOf: url)
-          let image = UIImage(data:imageData!)
-          self.postSpotImage = image!
+        if(self.postTrueSpotImagePath == ""){
+          self.imageUrl(imageURL: self.postTrueSpotImagePath)
+//          let url = URL(string: self.postTrueSpotImagePath)!
+//          let imageData = try? Data(contentsOf: url)
+//          DispatchQueue.main.async {
+//            let image = UIImage(data:imageData!)
+//            self.postSpotImage = image!
+//          }
         }else{
           self.postSpotImage = UIImage(named: "no-image.png")!
         }
@@ -897,6 +927,20 @@ class GlobalVar{
         print("検索お気に入り",self.searchFavoriteCountList)
       }
       }.resume()
+  }
+  func imageUrl(imageURL:String){
+    let url = URL(string: imageURL)
+    URLSession.shared.dataTask(with: url!, completionHandler: { (data, response, error) in
+      
+      if error != nil {
+        print(error!)
+        return
+      }
+      
+      DispatchQueue.main.async {
+        self.postSpotImage = UIImage(data: data!)!
+      }
+    }).resume()
   }
 }
 
