d4c3cb4d2b632a412aab652aacc0150a42b7ef01
inoka <is091160@gmail.com>
2019-02-20 14:14:42 +0900

    ユーザー詳細の他人ユーザー機能を追加修正

--- Journey/DetailUserViewController.swift
+++ Journey/DetailUserViewController.swift
@@ -38,6 +38,21 @@ class DetailUserViewController: UIViewController, UITabBarDelegate {
   var userIcon = UIImage()
   var userHeader = UIImage()
   
+  var planDate = ""
+  var planId = 0
+  var planTitle = ""
+  var planComment = ""
+  var price = ""
+  var area = ""
+  var transportation = ""
+  var favoriteCount = 0
+  var spotImagePath = ""
+  var spotImage = UIImage()
+  var spotNameA = ""
+  var spotNameB = ""
+  var spotCount = 0
+  var trueSpotImagePath = ""
+  
   var userPlanView = TopView(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
   var imgView:UIImageView!
   let myFrameSize:CGSize = UIScreen.main.bounds.size
@@ -71,6 +86,7 @@ class DetailUserViewController: UIViewController, UITabBarDelegate {
         userCommentTextView.numberOfLines = 0
         userCommentTextView.sizeToFit()
       }else{
+        userId = globalVar.userId
         imgView.image = globalVar.userIcon
         headerImageView.image = globalVar.userHeader
         subView.addSubview(self.imgView)
@@ -81,6 +97,8 @@ class DetailUserViewController: UIViewController, UITabBarDelegate {
         userCommentTextView.numberOfLines = 0
         userCommentTextView.sizeToFit()
       }
+      
+      userPostPlan(userId: userId)
       
       userPlanView = TopView(frame: CGRect(x: 16, y: pastLabel.frame.origin.y + pastLabel.frame.size.height + 8, width: UIScreen.main.bounds.size.width - 32, height: 150))
       userPlanView.backgroundColor = UIColor.red
@@ -203,6 +221,222 @@ class DetailUserViewController: UIViewController, UITabBarDelegate {
     }.resume()
   }
   
+//  struct PlanData : Codable{
+//    let status : Int
+//    let record : [Record]?
+//    let message : String?
+//    enum CodingKeys: String, CodingKey {
+//      case status
+//      case record
+//      case message
+//    }
+//    struct Record : Codable{
+//      let planDate : String
+//      let planId : Int
+//      let userId : String
+//      let planTitle : String
+//      let transportation : String
+//      let planComment : String
+//      let price : String
+//      let area : String
+//      enum CodingKeys: String, CodingKey {
+//        case planDate = "plan_date"
+//        case planId = "plan_id"
+//        case userId = "user_id"
+//        case planTitle = "plan_title"
+//        case planComment = "plan_comment"
+//        case transportation = "transportation"
+//        case price = "price"
+//        case area = "area"
+//      }
+//    }
+//  }
+//
+//  func getPlan(){
+//    //    var text = "http://\(globalVar.ipAddress)/api/v1/user/find?user_id=\(userId)"
+//    let url = URL(string: "http://\(globalVar.ipAddress)/api/v1/plan/find?user_id=\(userId)")
+//    //    text = text.addingPercentEncoding(withAllowedCharacters: NSCharacterSet.urlQueryAllowed)!
+//    //    //    let decodedString:String = text.removingPercentEncoding!
+//    //    print("URLのテスト", text)
+//    //    let url = URL(string: text)!
+//    let request = URLRequest(url: url!)
+//    let session = URLSession.shared
+//    session.dataTask(with: request) { (data, response, error) in
+//      if error == nil, let data = data, let response = response as? HTTPURLResponse {
+//        // HTTPヘッダの取得
+//        print("Content-Type: \(response.allHeaderFields["Content-Type"] ?? "")")
+//        // HTTPステータスコード
+//        print("statusCode: \(response.statusCode)")
+//        print(String(data: data, encoding: String.Encoding.utf8) ?? "")
+//        let planData = try? JSONDecoder().decode(PlanData.self, from: data)
+//        if(planData!.status == 200){
+//          for i in 0 ... (planData?.record?.count)! - 1{
+//            self.planDate = (planData?.record![i].planDate)!
+//            self.planId = (planData?.record![i].planId)!
+//            self.userId = (planData?.record![i].userId)!
+//            self.planTitle = (planData?.record![i].planTitle)!
+//            self.planComment = (planData?.record![i].planComment)!
+//            self.transportation = (planData?.record![i].transportation)!
+//            self.price = (planData?.record![i].price)!
+//            self.area = (planData?.record![i].area)!
+//          }
+//
+//          DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.1) {
+//            self.userPlanView.planNameLabel.text = self.planTitle
+//            self.userPlanView.planNameLabel.text = self.userGender
+//            self.userPlanView.commentLabel.text = self.userComment
+//            self.userGenerationTextView.text = "\(self.userGeneration)代"
+//            self.imgView.image = self.userIcon
+//            self.headerImageView.image = self.userHeader
+//          }
+//        }else{
+//          print("status",planData!.status)
+//        }
+//      }
+//      }.resume()
+//  }
+  
+  struct TimelineData : Codable{
+    let status : Int
+    let record : [Record]?
+    let message : String?
+    enum CodingKeys: String, CodingKey {
+      case status
+      case record
+      case message
+    }
+    struct Record : Codable{
+      let planId : Int
+      let userId : String
+      let planTitle : String
+      let planComment : String?
+      let transportation : String
+      let price : String
+      let area : String
+      let planDate : String
+      let user : User
+      let spots : [Spots]
+      let date : Date? = NSDate() as Date
+      enum CodingKeys: String, CodingKey {
+        case planId = "plan_id"
+        case userId = "user_id"
+        case planTitle = "plan_title"
+        case planComment = "plan_comment"
+        case transportation = "transportation"
+        case price = "price"
+        case area = "area"
+        case planDate = "plan_date"
+        case user = "user"
+        case spots = "spots"
+        case date
+      }
+      struct User : Codable{
+        let userName : String
+        let userIcon : String?
+        enum CodingKeys: String, CodingKey {
+          case userName = "user_name"
+          case userIcon = "user_icon"
+        }
+      }
+      struct Spots : Codable{
+        let spotId : Int
+        let spotTitle : String
+        let spotImageA : String?
+        let spotImageB : String?
+        let spotImageC : String?
+        enum CodingKeys: String, CodingKey {
+          case spotId = "spot_id"
+          case spotTitle = "spot_title"
+          case spotImageA = "spot_image_a"
+          case spotImageB = "spot_image_b"
+          case spotImageC = "spot_image_c"
+        }
+      }
+    }
+  }
+  
+  func userPostPlan(userId : String){
+    print(userId)
+    let url = URL(string: "http://\(globalVar.ipAddress)/api/v1/timeline/find?user_id=\(userId)&limit=1")
+    let request = URLRequest(url: url!)
+    let session = URLSession.shared
+    session.dataTask(with: request) { (data, response, error) in
+      if error == nil, let data = data, let response = response as? HTTPURLResponse {
+        // HTTPヘッダの取得
+        print("Content-Type: \(response.allHeaderFields["Content-Type"] ?? "")")
+        // HTTPステータスコード
+        print("statusCode: \(response.statusCode)")
+        print(String(data: data, encoding: String.Encoding.utf8) ?? "")
+        let timelineData = try? JSONDecoder().decode(TimelineData.self, from: data)
+        self.planId = (timelineData?.record![0].planId)!
+        self.userId = (timelineData?.record![0].userId)!
+        self.planTitle = (timelineData?.record![0].planTitle)!
+        self.userName = (timelineData?.record![0].user.userName)!
+        self.area = (timelineData?.record![0].area)!
+        self.transportation = (timelineData?.record![0].transportation)!
+        self.price = (timelineData?.record![0].price)!
+        self.planComment = (timelineData?.record![0].planComment)!
+        self.getFavoriteCount(planId: self.planId, flag: 2, updateFlag: 0, number: 0)
+        let planDate = (timelineData?.record![0].planDate)!.prefix(10)
+        var date = planDate.suffix(5)
+        if let range = date.range(of: "-"){
+          date.replaceSubrange(range, with: "月")
+        }
+        self.planDate = "\(date)日"
+        for i in 0 ... (timelineData?.record![0].spots.count)! - 1{
+          if((timelineData?.record![0].spots[i].spotImageA)! != ""){
+            self.spotImagePath = (timelineData?.record![0].spots[i].spotImageA)!
+          }else if((timelineData?.record![0].spots[i].spotImageB)! != ""){
+            self.spotImagePath = (timelineData?.record![0].spots[i].spotImageB)!
+          }else if((timelineData?.record![0].spots[i].spotImageC)! != ""){
+            self.spotImagePath = (timelineData?.record![0].spots[i].spotImageC)!
+          }
+          if(i == 0){
+            self.spotNameA = timelineData!.record![0].spots[i].spotTitle
+          }else if(i == 1){
+            self.spotNameB = (timelineData?.record![0].spots[i].spotTitle)!
+          }else if (i > 1){
+            self.spotCount += 1
+          }
+        }
+        
+        self.trueSpotImagePath = self.spotImagePath
+        if(self.trueSpotImagePath != ""){
+          let url = URL(string: self.trueSpotImagePath)!
+          let imageData = try? Data(contentsOf: url)
+          DispatchQueue.main.async {
+            let image = UIImage(data:imageData!)
+            self.spotImage = image!
+          }
+        }else{
+          self.spotImage = UIImage(named: "no-image.png")!
+        }
+        self.spotImagePath = ""
+        
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.1) {
+          self.userPlanView.planNameLabel.text = self.planTitle
+          self.userPlanView.planUserNameLabel.text = self.userName
+          self.userPlanView.planSpotNameLabel1.text = self.spotNameA
+          self.userPlanView.planSpotNameLabel2.text = self.spotNameB
+          if(self.spotCount == 0){
+            self.userPlanView.otherSpotLabel.text = ""
+          }else{
+            self.userPlanView.otherSpotLabel.text = "他\(self.spotCount)件"
+          }
+          self.userPlanView.planDateLabel.text = self.planDate
+          self.userPlanView.planImageView.image = self.spotImage
+          self.userPlanView.planUserIconImageView.image = self.imgView.image
+          self.userPlanView.planFavoriteLabel.text = self.favoriteCount.description
+          self.userPlanView.planUserIconImageView.layer.cornerRadius = 40 * 0.5
+          self.userPlanView.planUserIconImageView.clipsToBounds = true
+          self.userPlanView.planUserIconImageView.isUserInteractionEnabled = true
+          self.userPlanView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(DetailUserViewController.userPlanTapped(_ :))))
+//          self.newScroll.addSubview(userPlanView)
+        }
+      }
+    }.resume()
+  }
+  
   func createTabBar(){
     let width = self.view.frame.width
     let height = self.view.frame.height
@@ -278,23 +512,78 @@ class DetailUserViewController: UIViewController, UITabBarDelegate {
     if(segue.identifier == "toTimelineView" && timelineFlag == 1){
       let nextViewController = segue.destination as! TimelineViewController
       nextViewController.detailUserFlag = timelineFlag
-      nextViewController.detailUserId = globalVar.userId
+      nextViewController.detailUserId = userId
     }else if(segue.identifier == "toTimelineView" && favoriteFlag == 1){
       let nextView = segue.destination as! TimelineViewController
       nextView.favoriteFlag = 1
-//    }else if(segue.identifier == "toDetailPlanView"){
-//      let nextView = segue.destination as! DatailPlanViewController
-//      nextView.planId = self.globalVar.postPlanId
-//      nextView.userId = userId
-//      nextView.userName = userName
-//      nextView.userImage = self.imgView.image!
-//      nextView.planTitle = self.userPlanView.planNameLabel.text!
-//      nextView.planArea = self.globalVar.postPlanArea
-//      nextView.planTransportationString = self.globalVar.plan
-//      nextView.planComment = planComment
-//      nextView.planPrice = planPrice
+    }else if(segue.identifier == "toDetailPlanView"){
+      let nextView = segue.destination as! DatailPlanViewController
+      nextView.planId = self.planId
+      nextView.userId = userId
+      nextView.userName = userName
+      nextView.userImage = self.imgView.image!
+      nextView.planTitle = self.planTitle
+      nextView.planArea = self.area
+      nextView.planTransportationString = self.transportation
+      nextView.planComment = self.planComment
+      nextView.planPrice = self.price
     }
   }
+  
+  struct FavoriteData : Codable{
+    let status : Int?
+    let record : [Record]?
+    let message : String?
+    enum CodingKeys: String, CodingKey {
+      case status
+      case record
+      case message
+    }
+    struct Record : Codable{
+      //      let count : Int?
+      let rows : [Rows]?
+      //      let date : Date? = NSDate() as Date
+      enum CodingKeys: String, CodingKey {
+        //        case count = "count"
+        case rows = "rows"
+        //        case date = "date"
+      }
+      struct Rows : Codable{
+        let favoriteDate : String?
+        let planId : Int?
+        let userId : String?
+        enum CodingKeys: String, CodingKey {
+          case favoriteDate = "fav_date"
+          case planId = "plan_id"
+          case userId = "user_id"
+        }
+      }
+    }
+  }
+  
+  func getFavoriteCount(planId:Int, flag:Int, updateFlag:Int, number:Int){
+    let url = URL(string: "http://\(globalVar.ipAddress)/api/v1/favorite/find?plan_id=\(planId)")
+    let request = URLRequest(url: url!)
+    let session = URLSession.shared
+    session.dataTask(with: request) { (data, response, error) in
+      if error == nil, let data = data, let response = response as? HTTPURLResponse {
+        // HTTPヘッダの取得
+        print("Content-Type: \(response.allHeaderFields["Content-Type"] ?? "")")
+        // HTTPステータスコード
+        print("statusCode: \(response.statusCode)")
+        print(String(data: data, encoding: String.Encoding.utf8) ?? "")
+        let favoriteData = try! JSONDecoder().decode(FavoriteData.self, from: data)
+        if(favoriteData.status == 200){
+          self.favoriteCount = favoriteData.record!.count
+        }
+      }
+    }.resume()
+  }
+  
+  @objc func userPlanTapped(_ sender: UITapGestureRecognizer) {
+    performSegue(withIdentifier: "toDetailPlanView", sender: 0)
+  }
+
   
 //  @objc func userPlanTapped(_ sender: UITapGestureRecognizer) {
 //    let currentPage = pageControl.currentPage
